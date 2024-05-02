73d54a3467979296866f41b2dc65ffcf79f2d715
NoA <noa20040831@gmail.com>
2024-05-03 01:00:41 +0900

    汎用的に使えるボタンの実装

---
+++ WitchParty/Button.cpp
@@ -0,0 +1,23 @@
+﻿#include "button.hpp"
+
+
+
+Button::Button(const Vec2& _center_pos, const Size _size, const String& _text, const String& _font_asset_name,const Color& _text_color, const Color& _background_color, const Color& _border_color, const int& _border_stroke_width)
+	: center_pos { _center_pos },
+	size{ _size },
+	text{ _text },
+	font_asset_name { _font_asset_name },
+	text_color{ _text_color },
+	background_color{ _background_color },
+	border_color{ _border_color },
+	border_stroke_width{ _border_stroke_width } {}
+
+void Button::draw() const {
+
+	RectF{{ center_pos.x - size.x / 2, center_pos.y - size.y / 2},size }.draw(background_color).drawFrame(border_stroke_width,border_color);
+	FontAsset(font_asset_name)(text).drawAt(center_pos,text_color);
+}
+
+bool Button::leftClicked() const {
+	return true;
+}
---
+++ WitchParty/Button.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+class Button {
+
+private:
+	Vec2 center_pos;
+	Size size;
+	int border_stroke_width;
+	String text;
+	String font_asset_name;
+	Color text_color;
+	Color background_color;
+	Color border_color;
+
+public:
+
+	Button(
+		const Vec2& _center_pos,
+		const Size _size,
+		const String& _text,
+		const String& _font_asset_name,
+		const Color& _text_color,
+		const Color& _background_color,
+		const Color& _border_color,
+		const int& _border_stroke_width
+	);
+
+	void draw() const;
+
+	bool leftClicked() const;
+};
