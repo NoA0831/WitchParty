a6c2d67e6fad0599b47891b9a91edc8a1523db85
NoA <noa20040831@gmail.com>
2024-05-03 11:00:39 +0900

    miniサイズのFontAssetsの追加

--- WitchParty/Main.cpp
+++ WitchParty/Main.cpp
@@ -11,6 +11,7 @@ void Main()
 
 	FontAsset::Register(U"title", 50, FontFilePath, FontStyle::Bitmap);
 	FontAsset::Register(U"standard", 20, FontFilePath, FontStyle::Bitmap);
+	FontAsset::Register(U"mini", 15, FontFilePath, FontStyle::Bitmap);
 
 	while (System::Update()) {
 		if (not manger.update()) {
