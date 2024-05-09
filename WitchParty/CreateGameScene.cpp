b72aaaa8600cc85966f7c439df60fd49b4cb530b
NoA <noa20040831@gmail.com>
2024-05-09 16:33:26 +0900

    キャラチップの一部分のみを描画するように変更

--- WitchParty/CreateGameScene.cpp
+++ WitchParty/CreateGameScene.cpp
@@ -18,8 +18,11 @@ void CreateGameScene::draw() const {
 	prev_character_button.draw();
 	next_character_button.draw();
 	game_start_button.draw();
-	back_title_button.draw();
+	//back_title_button.draw();
+	ClearPrint();
+	Print << now_select_charactor;
 
-	String assets_name = CHAR_IMG_PATHS[now_select_charactor].name;
-	TextureAsset(assets_name).scaled(2.0).drawAt(300, 300);
+	String assets_name = CHAR_IMG_PATHS[ now_select_charactor ].name;
+	TextureAsset(assets_name)( 0, 0, 32, 32).scaled(3.0).drawAt(300, 300);
+
 }
